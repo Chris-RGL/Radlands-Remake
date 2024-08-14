42fad645c93bf2b02934e0467d80bd17333b8002
Chris <cgallingerlong@gmail.com>
2024-08-14 13:12:26 -0700

    Updated card class to use super to avoid repeditiveness.

--- Cards.py
+++ Cards.py
@@ -1,39 +1,29 @@
 class Cards:
-    """Abstract base class"""
-    def __init__(self):
-        raise NotImplementedError("Cannot create card object here")
-        self.type = None
-        self.junk = None
-        self.name = None
-        self.trait = None
-        self.ability = None
-        self.camp_draw = 0
-        self.play_cost = 0
-        self.ability_cost = 0
-        self.event_queue = 0
-        self.purchase_cost = 0
+    """Base Class"""
+    def __init__(self, name = None, type = None, ability = None):
+        self.name = name
+        self.type = type
+        self.ability = ability
 
 class Camp(Cards):
-    def __init__(self, type: str, name: str, trait: str, ability: str,
-                 camp_draw: int, ability_cost: int):
-        self.type = type
-        self.name = name
+    def __init__(self, name: str, type: str, ability: str,
+                 trait: str, ability_cost: int, camp_draw: int):
+        super().__init__(name = name, type = type, ability = ability)
+
         self.trait = trait
-        self.ability = ability
-        self.camp_draw = camp_draw
         self.ability_cost = ability_cost
+        self.camp_draw = camp_draw
 
     def __str__(self) -> str:
         return f"Name: {self.name}\nType: {self.type}\nTrait: {self.trait}\n" \
                f"Ability: {self.ability}\nCamp Draw: {self.camp_draw}\nAbility Cost: {self.ability_cost}\n"
 
 class Event(Cards):
-    def __init__(self, type: str, name: str, ability: str,
-                 play_cost: int, junk: str, event_queue: int):
-        self.type = type
+    def __init__(self, name: str, type: str, ability: str,
+                 junk: str, play_cost: int, event_queue: int):
+        super().__init__(name = name, type = type, ability = ability)
+
         self.junk = junk
-        self.name = name
-        self.ability = ability
         self.play_cost = play_cost
         self.event_queue = event_queue
 
@@ -42,13 +32,12 @@ class Event(Cards):
                f"Ability: {self.ability}\nPlay Cost: {self.play_cost}\nEvent Queue: {self.event_queue}\n"
 
 class People(Cards):
-    def __init__(self, type: str, junk: str, name: str, trait: str,
-                 ability: str, play_cost: int, ability_cost: int):
-        self.type = type
+    def __init__(self, name: str, type: str, ability: str, junk: str,
+                 trait: str, play_cost: int, ability_cost: int):
+        super().__init__(name = name, type = type, ability = ability)
+
         self.junk = junk
-        self.name = name
         self.trait = trait
-        self.ability = ability
         self.play_cost = play_cost
         self.ability_cost = ability_cost
 
@@ -57,18 +46,16 @@ class People(Cards):
                f"Ability: {self.ability}\nPlay Cost: {self.play_cost}\nAbility Cost: {self.ability_cost}\n"
 
 class Special(Cards):
-    def __init__(self, type: str, name: str, purchase_cost: int, ability: str,
-                 trait: str, junk: str):
-        self.type = type
+    def __init__(self, name: str, type: str, ability: str,
+                 junk: str, trait: str, purchase_cost: int):
+        super().__init__(name = name, type = type, ability = ability)
+
         self.junk = junk
-        self.name = name
         self.trait = trait
-        self.ability = ability
         self.purchase_cost = purchase_cost
 
     def __str__(self) -> str:
         return f"Name: {self.name}\nType: {self.type}\nJunk: {self.junk}\nTrait: {self.trait}\n" \
                f"Ability: {self.ability}\nPurchase Cost: {self.purchase_cost}\n"
-
 
 
