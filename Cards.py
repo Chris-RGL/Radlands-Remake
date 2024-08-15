066cf84c3d001f92359dc8cc7a46d1d1422ae27e
Jswenson24 <114791544+Jswenson24@users.noreply.github.com>
2024-08-15 11:44:03 -0700

    Women scary

--- Cards.py
+++ Cards.py
@@ -1,6 +1,6 @@
 class Cards:
     """Base Class"""
-    # WOMEN SCARY
+    # WOMEN SCARy
     def __init__(self, name = None, type = None, ability = None):
         self.name = name
         self.type = type
